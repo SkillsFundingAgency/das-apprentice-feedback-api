7502fc77c813c1dfb52d32623d0998abd7071c10
Ben Stone <ben1.stone@education.gov.uk>
2022-02-04 11:35:51 +0000

    QF-207 Tidy startup

--- src/SFA.DAS.ApprenticeFeedback.Api/Startup.cs
+++ src/SFA.DAS.ApprenticeFeedback.Api/Startup.cs
@@ -42,9 +42,6 @@ namespace SFA.DAS.ApprenticeFeedback.Api
             Environment = environment;
         }
 
-        
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
@@ -65,12 +62,8 @@ namespace SFA.DAS.ApprenticeFeedback.Api
 
             services.AddApiAuthentication(azureAdConfiguration, Environment.IsDevelopment())
                 .AddApiAuthorization(Environment);
-          
-
-            
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
