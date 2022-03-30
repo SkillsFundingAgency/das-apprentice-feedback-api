e8360feefa729f29f23ab284f652d371c8bc1e5e
Sarah Coetzee <sarahdun289@gmail.com>
2022-03-30 12:08:18 +0100

    Get PostDeploymentScript to add default provider attributes

--- src/SFA.DAS.ApprenticeFeedback.Database/Tables/Attribute.sql
+++ src/SFA.DAS.ApprenticeFeedback.Database/Tables/Attribute.sql
@@ -1,5 +1,8 @@
 ﻿CREATE TABLE [dbo].[Attribute]
 (
-	[AttributeId] INT NOT NULL PRIMARY KEY, 
-    [AttributeName] NVARCHAR(50) NULL
+	[AttributeId] INT 
+				NOT NULL 
+				IDENTITY(1, 1)
+				PRIMARY KEY,
+    [AttributeName] NVARCHAR(100) NULL
 )
