0da508f6cdc1e770205739550c4e381175087fc8
Sarah Coetzee <sarahdun289@gmail.com>
2022-03-30 16:02:01 +0100

    Update AddDefualtProviderAttributes sql script

--- src/SFA.DAS.ApprenticeFeedback.Database/Scripts/AddDefaultProviderAttributes.sql
+++ src/SFA.DAS.ApprenticeFeedback.Database/Scripts/AddDefaultProviderAttributes.sql
@@ -1,114 +1,35 @@
-﻿--write sql script to populate provider attributes in the table
-SET IDENTITY_INSERT Attribute ON;
-GO
+﻿
+CREATE TABLE #TempAttributes
+(
+	[AttributeId] INT ,
+    [AttributeName] NVARCHAR(100)
+)
 
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '1', 'Organising well-structured training') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Organising well-structured training'
-	)
+INSERT INTO #TempAttributes VALUES 
+(1,'Organising well-structured training'),
+(2, 'Communicating clearly with you'),
+(3, 'Providing accessible training resources'), 
+(4, 'Balancing online learning with classroom-based training to suit your apprenticeship'),
+(5, 'Taking into account your previous learning'),
+(6, 'Helping you learn new skills and develop existing ones'),
+(7, 'Giving you relevant training that helps you perform your job better'),
+(8, 'Training that takes up at least 20% of your total apprenticeship time'),
+(9, 'Supporting you and your apprenticeship'),
+(10, 'Responding to your needs'),
+(11, 'Ensuring you understand the requirements of your end-point assessment'),
+(12, 'Preparing you for success in your future career') 
 
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '2', 'Communicating clearly with you') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Communicating clearly with you'
-	)
+SET IDENTITY_INSERT [dbo].[Attribute] ON;
 
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '3', 'Providing accessible training resources') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Providing accessible training resources'
-	)
+MERGE Attribute TARGET
+USING #TempAttributes SOURCE
+ON TARGET.AttributeId=SOURCE.AttributeId
+WHEN MATCHED THEN
+UPDATE SET TARGET.AttributeName = SOURCE.AttributeName
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (AttributeId,AttributeName)
+VALUES (SOURCE.AttributeId,SOURCE.AttributeName);
 
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '4', 'Balancing online learning with classroom-based training to suit your apprenticeship') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Balancing online learning with classroom-based training to suit your apprenticeship'
-	)
+SET IDENTITY_INSERT [dbo].[Attribute] OFF;
+DROP TABLE #TempAttributes
 
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '5', 'Taking into account your previous learning') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Taking into account your previous learning'
-	)
-
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '6', 'Helping you learn new skills and develop existing ones') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Helping you learn new skills and develop existing ones'
-	)
-
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '7', 'Giving you relevant training that helps you perform your job better') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Giving you relevant training that helps you perform your job better'
-	)
-
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '8', 'Training that takes up at least 20% of your total apprenticeship time') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Training that takes up at least 20% of your total apprenticeship time'
-	)
-
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '9', 'Supporting you and your apprenticeship') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Supporting you and your apprenticeship'
-	)
-
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '10', 'Responding to your needs') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Responding to your needs'
-	)
-
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '11', 'Ensuring you understand the requirements of your end-point assessment') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Ensuring you understand the requirements of your end-point assessment'
-	)
-
-INSERT INTO Attribute(AttributeId, AttributeName)
-SELECT *
-FROM (SELECT '12', 'Preparing you for success in your future career') AS tmp(AttributeId, AttributeName)
-WHERE NOT EXISTS (
-	SELECT AttributeName 
-	FROM Attribute
-	WHERE AttributeName = 'Preparing you for success in your future career'
-	)
-
-SET IDENTITY_INSERT Attribute OFF;
-GO
