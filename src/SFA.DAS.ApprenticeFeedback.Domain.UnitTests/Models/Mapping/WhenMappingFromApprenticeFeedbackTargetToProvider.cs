162b7266c5149929161a86343cada6afecccb620
Matt <matt.daniels@digital.education.gov.uk>
2022-05-04 13:23:45 +0100

    Populate timewindow and significantdate on trainingprovider

--- src/SFA.DAS.ApprenticeFeedback.Domain.UnitTests/Models/Mapping/WhenMappingFromApprenticeFeedbackTargetToProvider.cs
+++ src/SFA.DAS.ApprenticeFeedback.Domain.UnitTests/Models/Mapping/WhenMappingFromApprenticeFeedbackTargetToProvider.cs
@@ -8,15 +8,15 @@ namespace SFA.DAS.ApprenticeFeedback.Domain.UnitTests.Models
     public class WhenMappingFromApprenticeFeedbackTargetToProvider
     {
         [Test, RecursiveMoqAutoData]
-        public void ThenTheFieldsAreCorrectlyMapped(ApprenticeFeedbackTarget source)
+        public void ThenTheFieldsAreCorrectlyMapped(ApprenticeFeedbackTarget source, ApplicationSettings appSettings)
         {
-            var result = (TrainingProvider)source;
+            var result = TrainingProvider.Create(source, appSettings);
 
             result.ApprenticeFeedbackTargetId.Should().Be(source.Id.Value);
             result.Ukprn.Should().Be(source.Ukprn);
             result.StartDate.Should().Be(source.StartDate);
             result.EndDate.Should().Be(source.EndDate);
-            result.ProviderName.Should().Be(source.ProviderName);
+            result.ProviderName.Should().Be(source.ProviderName);   
             result.Status.Should().Be(source.Status);
             result.FeedbackEligibility.Should().Be(source.FeedbackEligibility);
         }
--- src/SFA.DAS.ApprenticeFeedback.Domain/Models/TrainingProvider.cs
+++ src/SFA.DAS.ApprenticeFeedback.Domain/Models/TrainingProvider.cs
@@ -13,16 +13,17 @@ namespace SFA.DAS.ApprenticeFeedback.Domain.Models
         public string ProviderName { get; set; }
         public FeedbackTargetStatus Status { get; set; }
         public FeedbackEligibilityStatus FeedbackEligibility { get; set; }
+        public DateTime? SignificantDate { get; set; }
+        public TimeSpan? TimeWindow { get; set; }
 
-
-        public static implicit operator TrainingProvider(ApprenticeFeedbackTarget source)
+        public static TrainingProvider Create(ApprenticeFeedbackTarget source, ApplicationSettings appSettings)
         {
             if (source == null)
             {
                 return null;
             }
 
-            return new TrainingProvider
+            var trainingProvider = new TrainingProvider
             {
                 ApprenticeFeedbackTargetId = source.Id.GetValueOrDefault(Guid.Empty),
                 Ukprn = source.Ukprn.GetValueOrDefault(0),
@@ -32,6 +33,32 @@ namespace SFA.DAS.ApprenticeFeedback.Domain.Models
                 FeedbackEligibility = source.FeedbackEligibility,
                 Status = source.Status,
             };
+
+            if (FeedbackEligibilityStatus.Deny_HasGivenFeedbackRecently == trainingProvider.FeedbackEligibility)
+            {
+                trainingProvider.SignificantDate = source.LastFeedbackCompletedDate.Value.Date.AddDays(appSettings.RecentDenyPeriodDays);
+            }
+            else if (FeedbackEligibilityStatus.Deny_HasGivenFinalFeedback == trainingProvider.FeedbackEligibility)
+            {
+                trainingProvider.TimeWindow = new TimeSpan(days: appSettings.InitialDenyPeriodDays, 0, 0, 0);
+            }
+            else if (FeedbackEligibilityStatus.Deny_TooSoon == trainingProvider.FeedbackEligibility)
+            {
+                trainingProvider.TimeWindow = new TimeSpan(days: appSettings.InitialDenyPeriodDays, 0, 0, 0);
+                trainingProvider.SignificantDate = trainingProvider.StartDate.Date.AddDays(appSettings.InitialDenyPeriodDays);
+            }
+            else if (
+                (FeedbackEligibilityStatus.Deny_TooLateAfterPassing == trainingProvider.FeedbackEligibility)
+                ||
+                (FeedbackEligibilityStatus.Deny_TooLateAfterPausing == trainingProvider.FeedbackEligibility)
+                ||
+                (FeedbackEligibilityStatus.Deny_TooLateAfterWithdrawing == trainingProvider.FeedbackEligibility)
+                )
+            {
+                trainingProvider.TimeWindow = new TimeSpan(days: appSettings.FinalAllowedPeriodDays, 0, 0, 0);
+            }
+
+            return trainingProvider;
         }
     }
 }
