0fe1d72dc2fc58c581b6a6d44dc2173fdfe78d2b
Matt <matt.daniels@digital.education.gov.uk>
2022-06-14 15:57:39 +0100

    Fix code smells

--- src/SFA.DAS.ApprenticeFeedback.Domain/Interfaces/IApprenticeFeedbackRepository.cs
+++ src/SFA.DAS.ApprenticeFeedback.Domain/Interfaces/IApprenticeFeedbackRepository.cs
@@ -13,15 +13,6 @@ namespace SFA.DAS.ApprenticeFeedback.Domain.Interfaces
         // Apprentice Feedback Target
         Task<Guid?> CreateApprenticeFeedbackTarget(ApprenticeFeedbackTarget feedbackTarget);
         Task<ApprenticeFeedbackTarget> UpdateApprenticeFeedbackTarget(ApprenticeFeedbackTarget apprenticeFeedbackTarget);
-        /*
-         * As part of QF-428 we are moving away from using repos in Mediator request handlers.
-         * To achieve this refactoring, for data access - move functionality on to the context
-         * as discussed here:  https://lostechies.com/jimmybogard/2016/12/12/dealing-with-duplication-in-mediatr-handlers/
-        Task<ApprenticeFeedbackTarget> GetApprenticeFeedbackTargetById(Guid apprenticeFeedbackTargetId);
-        Task<ApprenticeFeedbackTarget> GetApprenticeFeedbackTarget(Guid apprenticeId, long commitmentApprenticeshipId);
-        Task<IEnumerable<ApprenticeFeedbackTarget>> GetApprenticeFeedbackTargets(Guid apprenticeId);
-        Task<IEnumerable<ApprenticeFeedbackTarget>> GetApprenticeFeedbackTargets(Guid apprenticeId, long ukprn);
-        */
 
         // Apprentice Feedback Result
         Task<ApprenticeFeedbackResult> CreateApprenticeFeedbackResult(ApprenticeFeedbackResult feedbackResult);
