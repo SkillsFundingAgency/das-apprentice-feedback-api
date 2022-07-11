c876b00748c313b6391d7903f3a8c23c8f829b06
Matt <matt.daniels@digital.education.gov.uk>
2022-07-11 17:14:15 +0100

    Refactor WhenRequestingFeedbackAttributes test

--- src/SFA.DAS.ApprenticeFeedback.Application.UnitTests/Queries/WhenRequestingFeedbackAttributes.cs
+++ src/SFA.DAS.ApprenticeFeedback.Application.UnitTests/Queries/WhenRequestingFeedbackAttributes.cs
@@ -1,11 +1,8 @@
 ﻿using AutoFixture.NUnit3;
 using FluentAssertions;
-using Moq;
 using NUnit.Framework;
 using SFA.DAS.ApprenticeFeedback.Application.Queries.GetAttributes;
-using SFA.DAS.ApprenticeFeedback.Application.UnitTests.MockHelpers;
-using SFA.DAS.ApprenticeFeedback.Domain.Interfaces;
-using SFA.DAS.Testing.AutoFixture;
+using SFA.DAS.ApprenticeFeedback.Data;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,20 +11,19 @@ namespace SFA.DAS.ApprenticeFeedback.Application.UnitTests.Queries
 {
     public class WhenRequestingFeedbackAttributes
     {
-        /*
-        [Test, RecursiveMoqAutoData]
+        [Test, AutoMoqData]
         public async Task ThenAttributesAreReturned(
             GetAttributesQuery query,
-            [Frozen] Mock<IAttributeContext> mockContext,
+            [Frozen(Matching.ImplementedInterfaces)] ApprenticeFeedbackDataContext context,
             GetAttributesQueryHandler handler,
             List<Domain.Entities.Attribute> response)
         {
-            mockContext.Setup(s => s.Entities).Returns(QueryableMockDbSet.Create(response.ToArray()));
+            context.Attributes.AddRange(response);
+            context.SaveChanges();
 
             var result = await handler.Handle(query, CancellationToken.None);
 
             result.ProviderAttributes.Count.Should().Be(response.Count);
         }
-        */
     }
 }
