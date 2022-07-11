3568f04f441e37f8efc5377f628d58bafc5ff9f5
codewolf86 <chris@codewolfconsulting.com>
2022-07-11 16:27:25 +0100

    Update Unit Tests

--- src/Tests/SFA.DAS.ApprenticeFeedback.Api.UnitTests/Controllers/CreateApprenticeFeedback/WhenPostingCreateApprenticeFeedback.cs
+++ src/Tests/SFA.DAS.ApprenticeFeedback.Api.UnitTests/Controllers/CreateApprenticeFeedback/WhenPostingCreateApprenticeFeedback.cs
@@ -1,16 +1,16 @@
-﻿using NUnit.Framework;
-using SFA.DAS.Testing.AutoFixture;
-using System.Threading.Tasks;
-using SFA.DAS.ApprenticeFeedback.Application.Commands.CreateApprenticeFeedback;
-using SFA.DAS.ApprenticeFeedback.Api.Controllers;
-using AutoFixture.NUnit3;
+﻿using AutoFixture.NUnit3;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System.Threading;
-using System;
+using NUnit.Framework;
+using SFA.DAS.ApprenticeFeedback.Api.Controllers;
+using SFA.DAS.ApprenticeFeedback.Application.Commands.CreateApprenticeFeedback;
 using SFA.DAS.ApprenticeFeedback.Application.Commands.CreateApprenticeFeedbackTarget;
+using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.ApprenticeFeedback.Api.UnitTests.Controllers.CreateApprenticeFeedback
 {
@@ -18,27 +18,26 @@ namespace SFA.DAS.ApprenticeFeedback.Api.UnitTests.Controllers.CreateApprenticeF
     {
         [Test, MoqAutoData]
         public async Task And_MediatorCommandIsSuccessful_Then_ReturnOk
-            (CreateApprenticeFeedbackTargetCommand request,
-            [Greedy] ApprenticeFeedbackTargetController controller)
+            (CreateApprenticeFeedbackCommand request,
+            [Greedy] ApprenticeFeedbackResultController controller)
         {
-            var result = await controller.Create(request);
+            var result = await controller.Post(request);
 
             result.Should().BeOfType<OkObjectResult>();
         }
 
-        /*
+        
         [Test, MoqAutoData]
         public async Task And_MediatorCommandIsUnsuccessful_Then_ReturnBadRequest
-            (CreateApprenticeFeedbackTargetCommand request,
+            (CreateApprenticeFeedbackCommand request,
             [Frozen] Mock<IMediator> mediator,
-            [Greedy] ApprenticeFeedbackTargetController controller)
+            [Greedy] ApprenticeFeedbackResultController controller)
         {
             mediator.Setup(m => m.Send(It.IsAny<CreateApprenticeFeedbackCommand>(), It.IsAny<CancellationToken>())).Throws(new Exception());
 
-            var result = await controller.Create(request);
+            var result = await controller.Post(request);
 
             result.Should().BeOfType<BadRequestResult>();
         }
-        */
     }
 }
