3880913a6fd466c265ddcf5e488d1234a2c89022
codewolf86 <chris@codewolfconsulting.com>
2022-07-12 14:14:39 +0100

    Update Provider Rating Count to int

--- src/SFA.DAS.ApprenticeFeedback.Domain/Entities/FeedbackForProvidersResult.cs
+++ src/SFA.DAS.ApprenticeFeedback.Domain/Entities/FeedbackForProvidersResult.cs
@@ -7,7 +7,7 @@ namespace SFA.DAS.ApprenticeFeedback.Domain.Entities
     {
         public long Ukprn { get; set; }
         public string ProviderRating { get; set; }
-        public long ProviderRatingCount { get; set; }
+        public int ProviderRatingCount { get; set; }
         public string AttributeName { get; set; }
         public string Category { get; set; }
         public int ProviderAttributeAgree { get; set; }
