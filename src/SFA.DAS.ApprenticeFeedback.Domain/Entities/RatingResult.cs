517ce57b16b44f301d2f36c768b619829502b299
codewolf86 <chris@codewolfconsulting.com>
2022-07-18 12:25:20 +0100

    Remove obsolete files

--- src/SFA.DAS.ApprenticeFeedback.Domain/Entities/RatingResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SFA.DAS.ApprenticeFeedback.Domain.Entities
-{
-    public class RatingResult
-    {
-        public string Rating { get; set; }
-        public long Count { get; set; }
-    }
-}
