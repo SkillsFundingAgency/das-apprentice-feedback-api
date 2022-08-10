20d55c625711b9e1527e53a64280fda8a728dda3
Matt <matt.daniels@digital.education.gov.uk>
2022-08-10 08:37:14 +0100

    First new endpoint - GetFeedbackTransactionsToEmail

---
+++ src/SFA.DAS.ApprenticeFeedback.Application/Queries/GetFeedbackTransactionsToEmail/GetFeedbackTransactionsToEmailQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SFA.DAS.ApprenticeFeedback.Application.Queries.GetFeedbackTransactions
+{
+    public class GetFeedbackTransactionsToEmailQuery : IRequest<GetFeedbackTransactionsToEmailResponse>
+    {
+        public int BatchSize {  get; set; }
+    }
+}
