d44bcea475ea851be3fe64fa4322efaa5bae20f8
codewolf86 <chris@codewolfconsulting.com>
2022-08-18 11:13:55 +0100

    Match app commitments packages

--- src/SFA.DAS.ApprenticeFeedback.Api/Startup.cs
+++ src/SFA.DAS.ApprenticeFeedback.Api/Startup.cs
@@ -16,10 +16,10 @@ using SFA.DAS.ApprenticeFeedback.Api.Configuration;
 using SFA.DAS.ApprenticeFeedback.Data;
 using SFA.DAS.ApprenticeFeedback.Domain.Configuration;
 using SFA.DAS.Configuration.AzureTableStorage;
-using SFA.DAS.UnitOfWork.EntityFrameworkCore.DependencyResolution.Microsoft;
 using SFA.DAS.UnitOfWork.NServiceBus.Features.ClientOutbox.DependencyResolution.Microsoft;
 using System;
 using System.IO;
+using SFA.DAS.UnitOfWork.EntityFrameworkCore.DependencyResolution.Microsoft;
 
 namespace SFA.DAS.ApprenticeFeedback.Api
 {
