127e334a92745a212637b71c9ad191f0d028cc66
Matt <matt.daniels@digital.education.gov.uk>
2022-08-23 11:43:48 +0100

    Add validation for batchSize

--- src/SFA.DAS.ApprenticeFeedback.Api/AppStart/AddServiceRegistration.cs
+++ src/SFA.DAS.ApprenticeFeedback.Api/AppStart/AddServiceRegistration.cs
@@ -1,5 +1,7 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.ApprenticeFeedback.Application.Behaviours;
 using SFA.DAS.ApprenticeFeedback.Application.Commands.CreateApprenticeFeedback;
 using SFA.DAS.ApprenticeFeedback.Data;
 using SFA.DAS.ApprenticeFeedback.Domain.Interfaces;
@@ -20,6 +22,9 @@ namespace SFA.DAS.ApprenticeFeedback.Api.AppStart
             services.AddScoped<IProviderStarsSummaryContext>(s => s.GetRequiredService<ApprenticeFeedbackDataContext>());
             services.AddScoped<IFeedbackTransactionContext>(s => s.GetRequiredService<ApprenticeFeedbackDataContext>());
             services.AddScoped<IDateTimeHelper, UtcTimeProvider>();
+
+            services.AddValidatorsFromAssembly(typeof(Application.Queries.GetFeedbackTransactionsToEmail.GetFeedbackTransactionsToEmailQueryValidator).Assembly);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         }
     }
 }
