b3c5c8168bb2d75fd6a5c27cf3acf3ab495e9de8
codewolf86 <chris@codewolfconsulting.com>
2022-08-24 13:35:42 +0100

    Revert renaming

--- src/SFA.DAS.ApprenticeFeedback.Domain/Models/FeedbackTransactionToEmail.cs
+++ src/SFA.DAS.ApprenticeFeedback.Domain/Models/FeedbackTransactionToEmail.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.ApprenticeFeedback.Domain.Models
 {
     public class FeedbackTransactionToEmail
     {
-        public long Id { get; set; }
+        public long ApprenticeFeedbackTransactionId { get; set; }
         public Guid ApprenticeId { get; set; }
     }
 }
