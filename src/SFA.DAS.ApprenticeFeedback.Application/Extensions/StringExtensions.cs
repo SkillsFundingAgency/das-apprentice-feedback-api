a1dadd6dae5a4368bee1822d9dec9af80cc5dd9e
Saw Naw <saw.naw@digital.education.gov.uk>
2023-01-10 18:08:36 +0000

    Add Regex matching timeout to prevent DoS attacks and make SonarCloud happy

--- src/SFA.DAS.ApprenticeFeedback.Application/Extensions/StringExtensions.cs
+++ src/SFA.DAS.ApprenticeFeedback.Application/Extensions/StringExtensions.cs
@@ -1,13 +1,18 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace SFA.DAS.ApprenticeFeedback.Application.Extensions
 {
     public static class StringExtensions
     {
+        internal static TimeSpan LegalExitSurveyRegexMatchTimeout => regexLegalExitSurveyAnswerCharacters.MatchTimeout;
+
         // Only these characters are valid to appear in the answers in the exit survey.
         // Guarding against HTML or JS injection attack
-        private static readonly Regex regexLegalExitSurveyAnswerCharacters = new Regex("[^a-zA-Z '-]");
-
+        private static readonly Regex regexLegalExitSurveyAnswerCharacters = new Regex("[^a-zA-Z '-]",
+                                                                                       RegexOptions.None, 
+                                                                                       TimeSpan.FromMilliseconds(100));
+        
         public static string RemoveIllegalCharacters(this string str)
         {
             if (null == str) return str;
