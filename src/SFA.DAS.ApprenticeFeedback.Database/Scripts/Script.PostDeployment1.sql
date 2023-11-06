50ec137ea5b53f3e0462179aef2a8db2f35ea3c7
Chris Woodcock <chris@humesoftware.com>
2023-11-06 16:35:15 +0000

    Adding temporary drop tables to clean-up environments - this branch will be deployed only in AT, TEST and TEST2 environments

--- src/SFA.DAS.ApprenticeFeedback.Database/Scripts/Script.PostDeployment1.sql
+++ src/SFA.DAS.ApprenticeFeedback.Database/Scripts/Script.PostDeployment1.sql
@@ -17,3 +17,50 @@ Post-Deployment Script Template
 :r .\AddProviderExclusions.sql
 :r .\AddEngagementEmails.sql
 
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ApprenticeExitSurvey_20221118091741]') AND type in (N'U'))
+    DROP TABLE [dbo].[ApprenticeExitSurvey_20221118091741];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ApprenticeExitSurvey_20221118111008]') AND type in (N'U'))
+    DROP TABLE [dbo].[ApprenticeExitSurvey_20221118111008];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ApprenticeFeedback]') AND type in (N'U'))
+    DROP TABLE [dbo].[ApprenticeFeedback];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ApprenticeFeedbackTargets]') AND type in (N'U'))
+    DROP TABLE [dbo].[ApprenticeFeedbackTargets];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ClientOutboxData]') AND type in (N'U'))
+    DROP TABLE [dbo].[ClientOutboxData];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[dbo.ApprenticeExitSurvey]') AND type in (N'U'))
+    DROP TABLE [dbo].[dbo.ApprenticeExitSurvey];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Exclusions]') AND type in (N'U'))
+    DROP TABLE [dbo].[Exclusions];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[FeedbackEmailHistory]') AND type in (N'U'))
+    DROP TABLE [dbo].[FeedbackEmailHistory];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[FeedbackEmailTransaction]') AND type in (N'U'))
+    DROP TABLE [dbo].[FeedbackEmailTransaction];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[FeedbackEmailTransactions]') AND type in (N'U'))
+    DROP TABLE [dbo].[FeedbackEmailTransactions];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[FeedbackTarget]') AND type in (N'U'))
+    DROP TABLE [dbo].[FeedbackTarget];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[FeedbackTargets]') AND type in (N'U'))
+    DROP TABLE [dbo].[FeedbackTargets];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[OutboxData]') AND type in (N'U'))
+    DROP TABLE [dbo].[OutboxData];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ProviderAttributes]') AND type in (N'U'))
+    DROP TABLE [dbo].[ProviderAttributes];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Providers]') AND type in (N'U'))
+    DROP TABLE [dbo].[Providers];
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Standards]') AND type in (N'U'))
+    DROP TABLE [dbo].[Standards];
