e322d4e686fd92f197077029df28a45b1f149ba6
ratheeshri1984 <ratheeshri1984@gmail.com>
2024-06-12 08:16:32 +0100

    EC-531 new fields

--- src/SFA.DAS.ApprenticeFeedback.Database/Tables/ProviderAttributeSummary.sql
+++ src/SFA.DAS.ApprenticeFeedback.Database/Tables/ProviderAttributeSummary.sql
@@ -5,6 +5,7 @@
     Agree INT NOT NULL DEFAULT 0, 
     Disagree INT NOT NULL DEFAULT 0, 
     UpdatedOn Datetime2,
+	TimePeriod NVARCHAR(50) NULL,
 	PRIMARY KEY(Ukprn, AttributeId) 
 )    
 ​
--- src/SFA.DAS.ApprenticeFeedback.Database/Tables/ProviderStarsSummary.sql
+++ src/SFA.DAS.ApprenticeFeedback.Database/Tables/ProviderStarsSummary.sql
@@ -2,7 +2,8 @@
 (
     Ukprn BIGINT NOT NULL,
     ReviewCount INT NOT NULL DEFAULT 0,
-    Stars INT NOT NULL
+    Stars INT NOT NULL,
+    TimePeriod NVARCHAR(50) NULL,
     PRIMARY KEY (Ukprn)
 )​
 GO
