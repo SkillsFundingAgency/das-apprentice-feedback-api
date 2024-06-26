65288a88745ec0367bdb0dd616c878a24a16728e
David Rimar <david.rimar@digital.education.gov.uk>
2024-06-26 10:30:53 +0100

    EC-487 return all years from repository

--- src/SFA.DAS.ApprenticeFeedback.Domain/Interfaces/IProviderStarsSummaryContext.cs
+++ src/SFA.DAS.ApprenticeFeedback.Domain/Interfaces/IProviderStarsSummaryContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using SFA.DAS.ApprenticeFeedback.Domain.Constants;
 using SFA.DAS.ApprenticeFeedback.Domain.Entities;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +9,7 @@ namespace SFA.DAS.ApprenticeFeedback.Domain.Interfaces
     public interface IProviderStarsSummaryContext : IEntityContext<Domain.Entities.ProviderStarsSummary>
     {
         public async Task<IEnumerable<ProviderStarsSummary>> FindProviderStarsSummaryAnnual(long ukprn)
-            => await Entities.Where(r => r.Ukprn == ukprn && r.TimePeriod != ReviewDataPeriod.AggregatedData).ToListAsync();
+            => await Entities.Where(r => r.Ukprn == ukprn).ToListAsync();
 
         public async Task<ProviderStarsSummary> FindProviderStarsSummaryForAcademicYear(long ukprn, string acYear)
             => await Entities.Where(r => r.Ukprn == ukprn && r.TimePeriod == acYear).SingleOrDefaultAsync();
