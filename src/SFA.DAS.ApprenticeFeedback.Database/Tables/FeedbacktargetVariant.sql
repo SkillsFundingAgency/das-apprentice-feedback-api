35502dfbda70e4bff89a77f26c4fa8587a34fe6b
Karen Hanna <karen@laveryhanna.com>
2024-10-03 15:38:14 +0100

    removed whitespace

--- src/SFA.DAS.ApprenticeFeedback.Database/Tables/FeedbacktargetVariant.sql
+++ src/SFA.DAS.ApprenticeFeedback.Database/Tables/FeedbacktargetVariant.sql
@@ -1,8 +1,6 @@
 ﻿CREATE TABLE [dbo].[FeedbackTargetVariant]
 (
-	[ApprenticeshipId] BIGINT NOT NULL PRIMARY KEY, 
+    [ApprenticeshipId] BIGINT NOT NULL PRIMARY KEY, 
     [Variant] VARCHAR(100) NOT NULL,
     [CreatedOn] DATETIME2(7) DEFAULT GETUTCDATE()
 )
-
-
