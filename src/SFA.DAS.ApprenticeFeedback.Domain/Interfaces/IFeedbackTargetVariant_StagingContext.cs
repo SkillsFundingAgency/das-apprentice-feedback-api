bc7082013aab7a2c6111b772209043d45eb9e4d9
Chris Woodcock <chris@humesoftware.com>
2024-11-13 17:36:00 +0000

    Reapply "Allow duplicates to be inserted when present in same batch"
    This reverts commit 1c30e6ec380a372e4cdeed844f1d28749d211130.

--- src/SFA.DAS.ApprenticeFeedback.Domain/Interfaces/IFeedbackTargetVariant_StagingContext.cs
+++ src/SFA.DAS.ApprenticeFeedback.Domain/Interfaces/IFeedbackTargetVariant_StagingContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using SFA.DAS.ApprenticeFeedback.Domain.Entities;
 using System.Collections.Generic;
 using System.Threading;
@@ -8,10 +9,26 @@ namespace SFA.DAS.ApprenticeFeedback.Domain.Interfaces
 {
     public interface IFeedbackTargetVariant_StagingContext : IEntityContext<FeedbackTargetVariant_Staging>
     {
+        ChangeTracker ChangeTracker { get; }
+
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 
-        public async Task AddRange(List<FeedbackTargetVariant_Staging> feedbackTargetVariants)
-            => await Entities.AddRangeAsync(feedbackTargetVariants);
+        public async Task AddRange(List<FeedbackTargetVariant_Staging> feedbackTargetVariants, CancellationToken cancellationToken = default)
+        {
+            var queryTrackingBehaviour = ChangeTracker.QueryTrackingBehavior;
+
+            try
+            {
+                ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+
+                await Entities.AddRangeAsync(feedbackTargetVariants);
+                await SaveChangesAsync(cancellationToken);
+            }
+            finally
+            {
+                ChangeTracker.QueryTrackingBehavior = queryTrackingBehaviour;
+            }
+        }
 
         Task ClearFeedbackTargetVariant_Staging();
     }
