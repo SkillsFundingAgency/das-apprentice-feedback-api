06ec86bb2b0fefcdece7efca11ead5f3d875820a
Phil Head <152269503+philhead1@users.noreply.github.com>
2025-07-17 11:27:31 +0100

    Update AddForeignKeyConstraintToFeedbackTransaction.sql
    Added condition to check to see whether the key already exists or not

--- src/SFA.DAS.ApprenticeFeedback.Database/Scripts/AddForeignKeyConstraintToFeedbackTransaction.sql
+++ src/SFA.DAS.ApprenticeFeedback.Database/Scripts/AddForeignKeyConstraintToFeedbackTransaction.sql
@@ -1,3 +1,4 @@
+IF(OBJECT_ID('FK_ApprenticeFeedbackTarget_FeedbackTransaction', 'F') IS NULL)
 ALTER TABLE [FeedbackTransaction]
 ADD CONSTRAINT FK_ApprenticeFeedbackTarget_FeedbackTransaction FOREIGN KEY (ApprenticeFeedbackTargetId)
 REFERENCES [ApprenticeFeedbackTarget] (Id);
